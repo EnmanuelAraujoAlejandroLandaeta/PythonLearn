66704ddf2f428ae1fb11bf29a83dd4b39f0fcdbc
Enmanuel Araujo <133147135+EnmanuelAraujoAlejandroLandaeta@users.noreply.github.com>
2024-02-20 22:28:20 -0400

    Add files via upload

---
+++ Clases objetos, atributos y metodos.py
@@ -0,0 +1,8 @@
+class Celular():
+    def __init__(self, marca, modelo, camara):
+        self.marca = marca
+        self.modelo = modelo
+        self.camara = camara
+
+celular1 = Celular("Samsung", "S23", "48MP")
+print(celular1)
